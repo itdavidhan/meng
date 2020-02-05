fb1bd484af5fe8dde309f8ee49d5ccf2d30ab532
davidhan <sword_13@163.com>
2020-02-05 17:31:18 +0800

    10个题型-矩阵网格

---
+++ src/components/tools/pcaPicker.vue
@@ -0,0 +1,183 @@
+<template>
+  <div class="city-picker-container">
+    <template
+      v-if="
+        cbData.level == 'province' ||
+          cbData.level == 'province_city' ||
+          cbData.level == 'province_city_county'
+      "
+    >
+      <span class="c-text" v-show="opt.showText">省：</span>
+      <Select
+        v-model="cpData.prov"
+        style="width: 150px; margin-right: 10px;"
+        @on-change="changeProvince"
+        :disabled="showFlag"
+        clearable
+        size="small"
+      >
+        <Option v-for="item in pcasData" :value="item.id" :key="item.id">{{
+          item.areaname
+        }}</Option>
+      </Select>
+    </template>
+    <template
+      v-if="
+        cbData.level == 'province_city' ||
+          cbData.level == 'province_city_county'
+      "
+    >
+      <span class="c-text" v-show="opt.showText">市：</span>
+      <Select
+        v-model="cpData.city"
+        style="width: 150px; margin-right: 10px;"
+        @on-change="changeCity"
+        :disabled="showFlag"
+        clearable
+        size="small"
+      >
+        <Option v-for="item in cityData" :value="item.id" :key="item.id">{{
+          item.areaname
+        }}</Option>
+      </Select>
+    </template>
+    <template v-if="cbData.level == 'province_city_county'">
+      <span class="c-text" v-show="opt.showText">区：</span>
+      <Select
+        v-model="cpData.area"
+        style="width:150px"
+        @on-change="changeArea"
+        :disabled="showFlag"
+        clearable
+        size="small"
+      >
+        <Option v-for="item in areaData" :value="item.id" :key="item.id">{{
+          item.areaname
+        }}</Option>
+      </Select>
+    </template>
+  </div>
+</template>
+<script>
+import pcas from "./china-area.json";
+// import pcas from './china-area-cdms.json'
+export default {
+  props: ["opt", "cp", "cbData", "disabled", "areaFlag"],
+  data() {
+    return {
+      pcasData: "",
+      provinceData: "",
+      cityData: "",
+      showFlag: false,
+      areaData: "",
+      cpData: {
+        prov: "",
+        city: "",
+        area: "",
+        prov_name: "",
+        city_name: "",
+        area_name: "",
+        cbData: null
+      }
+    };
+  },
+  methods: {
+    changeProvince(v) {
+      let currentPo = this._.filter(pcas, function(item) {
+        return item.id == v;
+      });
+      this.cityData = currentPo.length > 0 ? currentPo[0].city : [];
+      if (currentPo[0]) this.cpData.prov_name = currentPo[0].areaname;
+      if (
+        this.cbData.level == "province" &&
+        this.cpData.prov &&
+        this.cpData.prov.length > 0
+      ) {
+        this.$emit("getCpData", this.cpData);
+      }
+    },
+    changeCity(v) {
+      let currentCi = this._.filter(this.cityData, function(item) {
+        return item.id == v;
+      });
+      this.areaData = currentCi.length > 0 ? currentCi[0].area : [];
+      if (currentCi[0]) this.cpData.city_name = currentCi[0].areaname;
+      if (
+        this.cbData.level == "province_city" &&
+        this.cpData.city &&
+        this.cpData.city.length > 0
+      ) {
+        this.$emit("getCpData", this.cpData);
+      }
+    },
+    changeArea(v) {
+      let currentArea = this._.filter(this.areaData, function(item) {
+        return item.id == v;
+      });
+      if (currentArea[0]) this.cpData.area_name = currentArea[0].areaname;
+      if (
+        this.cbData.level == "province_city_county" &&
+        this.cpData.area &&
+        this.cpData.area.length > 0
+      ) {
+        this.$emit("getCpData", this.cpData);
+      }
+    },
+    // 编辑
+    initCp(param) {
+      let data = this._.cloneDeep(param);
+      if (typeof data == "undefined" || !data) return;
+      if (typeof data == "string") {
+        data = JSON.parse(data);
+      }
+      this.cpData = this._.cloneDeep(data);
+      this.cpData.cbData = this._.cloneDeep(this.cbData) || this.cpData.cbData;
+      this.changeProvince(data.prov);
+      if (
+        this.cbData.level == "province_city" ||
+        this.cbData.level == "province_city_county"
+      ) {
+        this.changeCity(data.city);
+      }
+      if (
+        this.cbData.level == "province" ||
+        this.cbData.level == "province_city" ||
+        this.cbData.level == "province_city_county"
+      ) {
+        this.changeArea(data.area);
+      }
+    }
+  },
+  created() {},
+  mounted() {
+    this.showFlag = this.disabled;
+    this.cpData.cbData = this.cbData || this.cpData.cbData;
+    this.pcasData = pcas;
+
+    if (this.cbData.level == "province_city_county")
+      this.changeArea(this.cpData.area);
+  },
+  watch: {
+    cp: {
+      handler(newVal) {
+        let vm = this;
+        (function() {
+          vm.initCp(newVal);
+        })();
+      },
+      deep: true
+    },
+    disabled(newVal) {
+      this.showFlag = newVal;
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+.city-picker-container {
+  display: inline-block;
+  .c-text {
+    padding-left: 10px;
+  }
+}
+</style>
--- src/pages/home/tableExample.vue
+++ src/pages/home/tableExample.vue
@@ -42,45 +42,70 @@
         <tr v-for="(item, index) in matrixData.table_row">
           <td>{{item.title}}</td>
           <td v-for="(oItem, oIndex) in item.questionOption">
-            <RadioGroup
-              v-model="oItem.value"
-              size="small"
-            >
-              <Radio
-                v-for="rItem in oItem.options"
-                :label="rItem.value"
-                >{{ rItem.title }}</Radio
+            <div v-if="oItem.question_type=='input'">
+              <Input v-model="oItem.value" size="small" style="width: 100%;"></Input>
+            </div>
+            <div v-if="oItem.question_type=='number'">
+              <InputNumber v-model="oItem.value" size="small" style="width: 200px;"></InputNumber>
+            </div>
+            <div v-if="oItem.question_type=='textarea'">
+              <Input type="textarea" v-model="oItem.value" size="small" style="width: 100%;"></Input>
+            </div>
+            <div v-if="oItem.question_type=='select'">
+              <Select v-model="oItem.value" style="width:200px" size="small">
+                <Option v-for="rItem in oItem.options" :value="rItem.value" :key="rItem.value">{{ rItem.title }}</Option>
+              </Select>
+            </div>
+            <div v-if="oItem.question_type=='radio'">
+              <RadioGroup
+                v-model="oItem.value"
+                size="small"
               >
-            </RadioGroup>
+                <Radio
+                  v-for="rItem in oItem.options"
+                  :label="rItem.value"
+                  >{{ rItem.title }}</Radio
+                >
+              </RadioGroup>
+            </div>
+            <div v-if="oItem.question_type=='checkbox'">
+              <CheckboxGroup v-model="oItem.value_list">
+                <Checkbox v-for="rItem in oItem.options" :label="rItem.value">{{rItem.title}}</Checkbox>
+              </CheckboxGroup>
+            </div>
+            <div v-if="oItem.question_type=='date'">
+              <DatePicker type="date" placeholder="请选择" :value="oItem.value" @on-change="oItem.value=$event" size="small"></DatePicker>
+            </div>
+            <div v-if="oItem.question_type=='datetime'">
+              <DatePicker type="datetime" placeholder="请选择" :value="oItem.value" @on-change="oItem.value=$event" size="small"></DatePicker>
+            </div>
+            <div v-if="oItem.question_type=='time'">
+              <TimePicker placeholder="请选择" :value="oItem.value" @on-change="oItem.value=$event" size="small"></TimePicker>
+            </div>
+            <div v-if="oItem.question_type=='address'">
+              <cityPicker
+                :cbData="{
+                  index,
+                  oIndex,
+                  level: oItem.options.view,
+                  absolute_id: oItem.field_code
+                }"
+                :opt="{ showText: false }"
+                @getCpData="getPCCData"
+              />
+            </div>
           </td>
           </RadioGroup>
         </tr>
       </table>
       <Button @click="saveData">保存矩阵网格数据</Button>
     </div>
-
-    <!-- <Table :columns="renderEasyDataColumn" :data="renderEasyData">
-      <template v-for="item in renderEasyDataColumn" slot-scope="{ row, index }" :slot="item.slot">
-        <div v-if="item.slot == 'title'">{{row.title}}</div>
-        <div v-else>{{row[item.slot]}}</div>
-        <div>
-          <RadioGroup
-            v-if="row[item.slot].question_type == 'radio'"
-            size="small"
-          >
-            <Radio
-              v-for="oItem in row[item.slot].options.options"
-              :label="oItem.value"
-              >{{ oItem.title }}</Radio
-            >
-          </RadioGroup>
-        </div>
-      </template>
-    </Table> -->
   </div>
 </template>
 <script>
+  import cityPicker from '@/components/tools/pcaPicker'
   export default {
+    components: {cityPicker},
     data() {
       return {
         // 单选网格题
@@ -169,15 +194,47 @@
         ],
         // 矩阵网格题
         matrixData: {//matrix_table矩阵网格
-          "table_row": [
-             {
+           "table_row": [
+              {
+                 "title": "姓名",
+                 "field_code": "100"
+              },
+              {
+                 "title": "年龄",
+                 "field_code": "101"
+              },
+              {
+                 "title": "性别",
+                 "field_code": "102"
+              },
+              {
+                 "title": "简介",
+                 "field_code": "103"
+              },
+              {
                 "title": "检查结果",
-                "field_code": "101"
-             },
-             {
+                "field_code": "104"
+              },
+              {
                 "title": "临床意义",
-                "field_code": "102"
-             }
+                "field_code": "105"
+              },
+              {
+                "title": "日期",
+                "field_code": "106"
+              },
+              {
+                "title": "日期时间",
+                "field_code": "107"
+              },
+              {
+                "title": "时间",
+                "field_code": "108"
+              },
+              {
+                "title": "省市区",
+                "field_code": "109"
+              },
            ],
            "table_column":[
              {   
@@ -193,31 +250,65 @@
            // "view":"column", //row行 column列
            "options": [
               {
-                 "title": "检查结果",
-                 "field_code": "101",
                  "descriptions": "字段备注",
                  "type": "var",  //字段类型 var
-                 "question_type": "radio",  //控件类型 radio/checkbox
+                 "question_type": "input",  //控件类型 radio/checkbox
+                 "field_length": "100", //字段长度var类型默认前端字段代入100 可修改1~1000        
+              },
+              {
+                 "descriptions": "字段备注",
+                 "type": "var",  //字段类型 var
+                 "question_type": "number",  //控件类型 radio/checkbox
+                 "field_length": "100", //字段长度var类型默认前端字段代入100 可修改1~1000        
+              },
+              {
+                 "descriptions": "字段备注",
+                 "type": "var",  //字段类型 var
+                 "question_type": "select",  //控件类型 radio/checkbox
+                 "field_length": "100", //字段长度var类型默认前端字段代入100 可修改1~1000 
+                 "options": [
+                     {
+                       "serial_number": '1',
+                       "options_absolute_id": '1',
+                       "title": "男",
+                       "value": '1',  //实际入库内容
+                     },
+                     {
+                       "serial_number": '2',
+                       "options_absolute_id": '2',
+                       "title": "女",
+                       "value": '2',  //实际入库内容
+                     }
+                 ]                        
+              },
+              {
+                 "descriptions": "字段备注",
+                 "type": "var",  //字段类型 var
+                 "question_type": "textarea",  //控件类型 radio/checkbox
+                 "field_length": "100", //字段长度var类型默认前端字段代入100 可修改1~1000        
+              },
+              {
+                 "descriptions": "字段备注",
+                 "type": "var",  //字段类型 var
+                 "question_type": "checkbox",  //控件类型 radio/checkbox
                  "field_length": "100", //字段长度var类型默认前端字段代入100 可修改1~1000
                  "view": "x", //x代表横向  y代表纵向
                  "options": [
                      {
-                       "serial_number": 1,
-                       "options_absolute_id": 1,
+                       "serial_number": '1',
+                       "options_absolute_id": '1',
                        "title": "正常",
-                       "value": 1,  //实际入库内容
+                       "value": '1',  //实际入库内容
                      },
                      {
-                       "serial_number": 2,
-                       "options_absolute_id": 2,
+                       "serial_number": '2',
+                       "options_absolute_id": '2',
                        "title": "不正常",
-                       "value": 2,  //实际入库内容
+                       "value": '2',  //实际入库内容
                      }
                  ]                      
               },
               {
-                 "title": "临床意义",
-                 "field_code": "102",
                  "descriptions": "字段备注",
                  "type": "var",  //字段类型 var
                  "question_type": "radio",  //控件类型 radio/checkbox
@@ -225,68 +316,60 @@
                  "view": "x", //x代表横向  y代表纵向
                  "options": [
                      {
-                       "serial_number": 1,
-                       "options_absolute_id": 1,
+                       "serial_number": '1',
+                       "options_absolute_id": '1',
                        "title": "正常",
-                       "value": 1,  //实际入库内容
+                       "value": '1',  //实际入库内容
                      },
                      {
-                       "serial_number": 2,
-                       "options_absolute_id": 2,
+                       "serial_number": '2',
+                       "options_absolute_id": '2',
                        "title": "异常无临床意义",
-                       "value": 2,  //实际入库内容
+                       "value": '2',  //实际入库内容
                      }
                  ]                      
-              } 
+              },
+              {
+                 "descriptions": "字段备注",
+                 "type": "var",  //字段类型 var
+                 "question_type": "date",  //控件类型 radio/checkbox
+                 "field_length": "100", //字段长度var类型默认前端字段代入100 可修改1~1000        
+              },
+              {
+                 "descriptions": "字段备注",
+                 "type": "var",  //字段类型 var
+                 "question_type": "datetime",  //控件类型 radio/checkbox
+                 "field_length": "100", //字段长度var类型默认前端字段代入100 可修改1~1000        
+              },
+              {
+                 "descriptions": "字段备注",
+                 "type": "var",  //字段类型 var
+                 "question_type": "time",  //控件类型 radio/checkbox
+                 "field_length": "100", //字段长度var类型默认前端字段代入100 可修改1~1000        
+              },
+              {
+                 "descriptions": "字段备注",
+                 "type": "var",  //字段类型 var
+                 "question_type": "address",  //控件类型 radio/checkbox
+                 "field_length": "100", //字段长度var类型默认前端字段代入100 可修改1~1000 
+                 "options": {
+                   "view": "province_city_county"
+                 }       
+              },
            ]
         },
-        columns1: [
-          {
-            title: 'Name',
-            key: 'name'
-          },
-          {
-            title: 'Age',
-            key: 'age'
-          },
-          {
-            title: 'Address',
-            slot: 'address'
-          }
-        ],
-        data1: [
-          {
-            name: 'John Brown',
-            age: 18,
-            address: {prov: '山东省', city: '济南市', 'area': '槐荫区'},
-            date: '2016-10-03'
-          },
-          {
-            name: 'Jim Green',
-            age: 24,
-            address: {prov: '山东省', city: '济南市', 'area': '槐荫区'},
-            date: '2016-10-01'
-          },
-          {
-            name: 'Joe Black',
-            age: 30,
-            address: {prov: '山东省', city: '济南市', 'area': '槐荫区'},
-            date: '2016-10-02'
-          },
-          {
-            name: 'Jon Snow',
-            age: 26,
-            address: {prov: '山东省', city: '济南市', 'area': '槐荫区'},
-            date: '2016-10-04'
-          }
-        ]
+        // 矩阵网格题-答案
+        matrixAnswer: {
+          // "101_201":{"absolute_id":"101_201","type":"checkbox","value_list":{"2":{"element_id":2,"title":"不正常","value":2}}},"101_202":{"absolute_id":"101_202","type":"checkbox","value_list":{"1":{"element_id":1,"title":"正常","value":1}}},"102_201":{"absolute_id":"102_201","type":"radio","value_list":{"1":{"element_id":1,"title":"正常","value":1}}},"102_202":{"absolute_id":"102_202","type":"radio","value_list":{"2":{"element_id":2,"title":"异常无临床意义","value":2}}}
+        },
       }
     },
     mounted() {
       this.transformMatrixData();
     },
     methods: {
-      // 将矩阵网格数据转换为页面渲染所需格式
+      // 第一步：获取后端数据
+      // 第二步：将矩阵网格数据转换为页面渲染所需格式
       transformMatrixData() {
         this.matrixData.table_row.forEach((rItem, rIndex)=>{
           let arr = [];
@@ -298,14 +381,52 @@
             let _options = this.matrixData.options[_index];
             _options.title = rItem.title;
             _options.field_code = r_code+'_'+c_code;
+            if(_options.question_type=='number') {
+              _options.value = null;
+            }
             arr.push(this._.cloneDeep(_options));
           })
           rItem.questionOption = this._.cloneDeep(arr);
         })
         this.matrixData = this._.cloneDeep(this.matrixData)
-        console.log(666, this.matrixData.table_row)
+        this.transformAnswer();
+        this.bindAnswer();
       },
-      // 保存矩阵网格数据
+      // 第三步：转换答案格式
+      transformAnswer() {
+        if(!this.matrixAnswer) return;
+        let _matrixAnswer = this._.cloneDeep(this.matrixAnswer);
+        for(let key in _matrixAnswer) {
+          let item = _matrixAnswer[key];
+          let type = item.type;
+          let arr= item.value_list ? Object.keys(item.value_list) : [];
+          switch(type) {
+            case 'input':
+              break;
+            case 'radio':
+            case 'select':
+              item.value = arr[0];
+              break;
+            case 'checkbox':
+              item.value_list = arr;
+              break;
+          }
+        }
+        this.matrixAnswer = this._.cloneDeep(_matrixAnswer)
+      },
+      // 第四步：绑定答案
+      bindAnswer() {
+        this.matrixData.table_row.forEach(item=>{
+          item.questionOption.forEach(oItem=>{
+            if(this.matrixAnswer[oItem.field_code]) {
+              oItem.value = this.matrixAnswer[oItem.field_code].value;
+            }
+          })
+        })
+        this.matrixData = this._.cloneDeep(this.matrixData)
+        console.log('this.matrixData', this.matrixData)
+      },
+      // 第五步：保存矩阵网格数据-将数据转为后端所需格式
       saveData() {
         let arr = [];
         let json = {};
@@ -316,24 +437,102 @@
         })
         console.log('arr', arr)
         arr.forEach(item=>{
-          json[item.field_code] = {
-            absolute_id: item.field_code,
-            type: item.question_type,
-            value_list: {}
-          };
-          let obj = {};
-          item.options.forEach(oItem=>{
-            if(oItem.value == item.value) {
-              obj = oItem;
-            }
-          })
-          json[item.field_code].value_list[item.value] = {
-            element_id: obj.options_absolute_id,
-            title: obj.title,
-            value: obj.value,
+          let type = item.question_type;
+          switch(type) {
+            case 'input':
+            case 'number':
+              json[item.field_code] = {
+                absolute_id: item.field_code,
+                type: item.question_type,
+                value: item.value,
+                value_unit: item.value_unit,
+              };
+              break;
+            case 'date':
+            case 'datetime':
+            case 'time':
+            case 'textarea':
+              json[item.field_code] = {
+                absolute_id: item.field_code,
+                type: item.question_type,
+                value: item.value,
+              };
+              break;
+            case 'address':
+              json[item.field_code] = {
+                absolute_id: item.field_code,
+                type: item.question_type,
+                value_list: item.value_list,
+              };
+              break;
+            case 'radio':
+            case 'select':
+              json[item.field_code] = {
+                absolute_id: item.field_code,
+                type: item.question_type,
+                value_list: {}
+              };
+              if(!item.value) return;
+              let obj = {};
+              item.options.forEach(oItem=>{
+                if(oItem.value == item.value) {
+                  obj = oItem;
+                }
+              })
+              json[item.field_code].value_list[item.value] = {
+                element_id: obj.options_absolute_id,
+                title: obj.title,
+                value: obj.value,
+              }
+              break;
+            case 'checkbox':
+              json[item.field_code] = {
+                absolute_id: item.field_code,
+                type: item.question_type,
+                value_list: {}
+              };
+              if(!item.value_list) return;
+              item.value_list.forEach(vItem=>{
+                let obj = {};
+                item.options.forEach(oItem=>{
+                  if(oItem.value == vItem) {
+                    obj = oItem;
+                  }
+                })
+                json[item.field_code].value_list[vItem] = {
+                  element_id: obj.options_absolute_id,
+                  title: obj.title,
+                  value: obj.value,
+                }
+              })
+              break;
           }
+          
         })
-        console.log('json', json)
+        console.log('json1', json)
+        console.log('json2', JSON.stringify(json) )
+      },
+      getPCCData(o) {
+        console.log('o', o)
+        let index = o.cbData.index;
+        let oIndex = o.cbData.oIndex;
+        this.matrixData.table_row[index].questionOption[oIndex].value_list={
+          province: { 
+            "element_id":"province", 
+            "title": o.prov_name, 
+            "value": o.prov, 
+          }, 
+          city: { 
+            "element_id":"city", 
+            "title": o.city_name, 
+            "value": o.city,
+          }, 
+          county: { 
+            "element_id":"county", 
+            "title": o.area_name, 
+            "value": o.area,
+          }
+        }
       },
     },
   }
@@ -350,8 +549,7 @@
       border: 1px solid #DDD;
       th, td {
         border: 1px solid #DDD;
-        padding-left: 10px;
-        padding-right: 10px;
+        padding: 5px;
       }
       .option-item {
         text-align: center;
